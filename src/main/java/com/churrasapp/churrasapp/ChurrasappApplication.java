33b3e6462523d6e9e95043f92a413561cf61dd7e
Debora Cervieri Guterres <debora.guterres@gmail.com>
2022-01-18 14:46:38 -0300

    First commit

---
+++ src/main/java/com/churrasapp/churrasapp/ChurrasappApplication.java
@@ -0,0 +1,13 @@
+package com.churrasapp.churrasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurrasappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurrasappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/churrasapp/churrasapp/ChurrasappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.churrasapp.churrasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurrasappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
