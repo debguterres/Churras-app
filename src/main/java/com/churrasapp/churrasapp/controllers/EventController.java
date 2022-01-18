ddcb84c168322bdbe519b7267860ae2a5ee0235d
Debora Cervieri Guterres <debora.guterres@gmail.com>
2022-01-18 15:17:49 -0300

    Created MVC layers

---
+++ src/main/java/com/churrasapp/churrasapp/controllers/EventController.java
@@ -0,0 +1,14 @@
+package com.churrasapp.churrasapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventController {
+
+    @RequestMapping("/cadastrarEvento")
+    public String form(){
+        return "evento/formEvento";
+    }
+    
+}
---
+++ src/main/java/com/churrasapp/churrasapp/models/Event.java
@@ -0,0 +1,17 @@
+package com.churrasapp.churrasapp.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Event {
+
+    private String nome;
+    private String local;
+    private String data;
+    private String horário;
+
+
+    
+}
